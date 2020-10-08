5aa12103c0821611d014f64dbf1aae7afa2e2610
DoobriN <72567268+DoobriN@users.noreply.github.com>
2020-10-08 19:33:07 +0300

    Update camera.js

--- camera.js
+++ camera.js
@@ -33,3 +33,6 @@ function capteaza()
 		ctx.drawImage(video, 0, 0, 640, 480);
 		
 }
+const cnvs = document.querySelector("video");
+cnvs.addEventListener('click',capteaza);
+cnvs.addEventListener('touch', capteaza);
